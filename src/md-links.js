b645f4ff775f19f99e0e9088078c8a894567e1c9
marisolcastrolopez <46985112+marisolcastrolopez@users.noreply.github.com>
2019-04-16 16:28:17 -0500

    Validate theres a path written by the user

--- src/md-links.js
+++ src/md-links.js
@@ -1,15 +1,81 @@
-var fs = require('fs');
+const isTherePath = (pathGiven) => {
+    if (pathGiven) {
+        return true;
+    } else {
+        console.log('Hint: Write a Path eg: documents/readme.md');
+        return false;
+    };
+}
 
 module.exports = {
-    validateAbsPath: (pathToTest) => { 
-        fs.stat(pathToTest, function (err) {
-        if (!err) {
-            console.log('OK :) File or directory exist');
-        }
-        else if (err.code === 'ENOENT') {
-            console.log('file or directory does not exist');
-        }
-    })
-},
-    validateRelPath: 1,
+    isTherePath
 }
+
+
+// var fs = require('fs');
+// var fileExistSync = require('./fileExistSync');
+
+// var exist = fileExistSync('/var/folders/zm/jmjb49l172g6g/T/65b199');
+
+// module.exports = fs.existsSync || function existsSync(filePath) {
+//     try {
+//         fs.statSync(filePath);
+//     } catch (err) {
+//         if (err.code == 'ENOENT') return false;
+//     }
+//     return true;
+// };
+
+
+// let fs = require('fs');
+// let filePath = process.argv[2];
+
+// fs.readFile(filePath, function callback(err, data) {
+//    if (err) {
+//        return err
+//    }
+
+//    let str = data.toString();
+//    let arr= str.split('\n');
+//    let l = arr.length-1;
+//    console.log(l);
+
+//  });
+// var fs = require('fs');
+// let path = require('path');
+
+
+
+// module.exports = {
+//     validateAbsPath: (pathToTest) => {
+//         fs.stat(pathToTest, function(err) {
+//             if (!err) {
+//                 console.log('file or directory exists');
+//             }
+//             else if (err.code === 'ENOENT') {
+//                 console.log('file or directory does not exist');
+//             }
+//         });
+// },
+//     validateRelPath: 1,
+// }
+
+
+// const findDirectory = (dirName, extStr, cb) =>{
+//     let arr=[];
+//     fs.readdir(dirName, function callback(err, data){
+//         if(err){return cb(err)}
+
+//         let l = data.length;
+//         for (let i = 0; i < l; i++){
+//             let x = data [i];
+//             let exten = path.extname(x);
+
+//             if(exten == '.'+ extStr){ arr.push(x);}
+//         }
+//         return cb(null, arr);
+//     })
+// }
+// module.exports = {
+//     findDirectory
+// }
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,24 +1,62 @@
-const mdLinks = require('../src/md-links.js');
-const path = require('path');
-const invalidAbsPath = path.basename('C:\\temp\\invalidabspath.html');
-const validAbsPath = path.basename('C:\\');
+const mdLinks = require('../src/md-links.js')
+const pathGiven = 'foo/bar';
 
-describe('validateAbsPath', () => {
+//Is there a path?
+describe('isTherePath', () => {
   it('it should be an function', () => {
-    expect(typeof (mdLinks.validateAbsPath)).toBe('function');
+    expect(typeof (mdLinks.isTherePath)).toBe('function');
   });
 
-  it('validateAbsPath should return an error with an invalid absolute path', () => {
-    expect(mdLinks.validateAbsPath(invalidAbsPath)).toEqual(
-      console.log('File or directory does not exist')
+  it('isTherePath should return OK statement if there\'s a path', () => {
+    expect(mdLinks.isTherePath(pathGiven)).toEqual(
+
+      true
     );
   });
 
-  it('validateAbsPath should return an OK message with an valid absolute path', () => {
-    expect(mdLinks.validateAbsPath(validAbsPath)).toEqual(
-      console.log('OK :) File or directory exist')
+  it('isTherePath should return an ERROR statement if theres not a path', () => {
+    expect(mdLinks.isTherePath()).toEqual(
+      false
     );
   });
 });
 
+//Is the path a directory or a file?
+describe('isTherePath', () => {
+  it('it should be an function', () => {
+    expect(typeof (mdLinks.isTherePath)).toBe('function');
+  });
+
+  it('isTherePath should return OK statement if theres a path', () => {
+    expect(mdLinks.isTherePath(pathGiven)).toEqual(
+      true
+    );
+  });
+
+  it('isTherePath should return an ERROR statement if theres not a path', () => {
+    expect(mdLinks.isTherePath()).toEqual(
+      false
+    );
+  });
+});
+
+
+
+// describe('validateAbsPath', () => {
+//   it('it should be an function', () => {
+//     expect(typeof (mdLinks.validateAbsPath)).toBe('function');
+//   });
+
+//   it('validateAbsPath should return an error with an invalid absolute path', () => {
+//     expect(mdLinks.validateAbsPath(invalidAbsPath)).toEqual(
+//       false
+//     );
+//   });
+
+//   it('validateAbsPath should return an OK message with an valid absolute path', () => {
+//     expect(mdLinks.validateAbsPath(validAbsPath)).toEqual(
+//       true
+//     );
+//   });
+// });
 

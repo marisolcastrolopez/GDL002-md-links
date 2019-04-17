1855d8327dda93047e12f7d2c10dd837386327c8
marisolcastrolopez <46985112+marisolcastrolopez@users.noreply.github.com>
2019-04-17 11:03:43 -0500

    Bin folder created

--- src/md-links.js
+++ src/md-links.js
@@ -80,3 +80,22 @@ module.exports = {
 // module.exports = {
 //     findDirectory
 // }
+
+// const {findDirectory} = require('../src/md-links.js');
+// const ext = '.md';
+// let dir = 'C:\\Users\\solca\\Documents\\Laboratoria\\mdlinks\\GDL002-md-links\\src';
+// let path = require('path');
+
+// console.log(findDirectory, ext, dir)
+
+// let callback = function(err, data){
+//     console.log(data);
+//     let dataJS = path.basename('C:\\Users\\solca\\Documents\\Laboratoria\\mdlinks\\GDL002-md-links\\src\\readme.md');
+//     if(err){console.log(err); return err}
+//     for(let i = 0; i< dataJS.length; i++){
+//         console.log(dataJS[i])
+        
+//     }
+// }
+
+// findDirectory(dir, ext, callback);
